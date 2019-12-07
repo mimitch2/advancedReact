636dd6fdc1019e76ca672f40c5b10ee4b257b8cf
mimitch2 <mike@lawnstarter.com>
2019-12-07 11:11:24 -0600

    pre graphQL

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,14 +1,25 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import NavStyles from './styles/NavStyles'
 
- const Nav = () => (
-        <div>
-                 <Link href="/sell">
-                <a>Sell</a>
-            </Link>
-            <Link href="/">
-                <a>Home</a>
-            </Link>
-        </div>
-    )
+const Nav = () => (
+    <NavStyles>
+        <Link href="/items">
+            <a>Items</a>
+        </Link>
+        <Link href="/sell">
+            <a>Sell</a>
+        </Link>
+        <Link href="/signup">
+            <a>Signup</a>
+        </Link>
+        <Link href="/orders">
+            <a>Orders</a>
+        </Link>
+        <Link href="/me">
+            <a>Account</a>
+        </Link>
 
-    export default Nav;
+    </NavStyles>
+);
+
+export default Nav;
